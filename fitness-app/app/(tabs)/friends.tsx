e129a554f5a360b243e55a4234e019b1ab8024b6
loka27e-cmd <eeshanloka@gmail.com>
2026-05-17 18:46:38 -0400

    styled the friends.tsx page to fit the original theme

--- fitness-app/app/(tabs)/friends.tsx
+++ fitness-app/app/(tabs)/friends.tsx
@@ -131,29 +131,29 @@ export default function Friends() {
 
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 24}}>Friends</Text>
+      <Text style={styles.head}>Friends</Text>
       {friends.map((friend) => (
-      <Text key={friend.friendID}>
-        {friend.username}
+      <Text style={styles.acDcTxt} key={friend.friendID}>
+        - {friend.username}
       </Text>
       ))}
-      <Text style={{fontSize: 20}}>Friend Request Recipient</Text>
-      <TextInput maxLength={20} onChangeText={onChangeRecipientText} value={recipient} placeholder="Enter here" />
+      <Text style={styles.mainText}>Friend Request Recipient</Text>
+      <TextInput maxLength={20} style={styles.inputText} onChangeText={onChangeRecipientText} value={recipient} placeholder="Enter here" />
       <TouchableHighlight onPress = {sendFriendRequest}>
-        <Text style={{fontSize: 20}}>Send Friend Request</Text>
+        <Text style={styles.mainText}>Send Friend Request</Text>
       </TouchableHighlight>
-      <Text style={{fontSize: 24}}>Friend Requests</Text>
+      <Text style={styles.mainText}>Friend Requests</Text>
 
         {requests.map((request: FriendRequest) => (
-          <View key={request.requesterId}>
-            <Text>
-              {request.requesterUsername}
+          <View key={request.requesterId} style={styles.reqText}>
+            <Text style={styles.acDcTxt}>
+              - {request.requesterUsername}
             </Text>
             <TouchableHighlight onPress={() => acceptFriendRequest(request.requesterId)}>
-              <Text>Accept</Text>
+              <Text style={styles.acDcTxt}>Accept</Text>
             </TouchableHighlight>
             <TouchableHighlight onPress={() => declineFriendRequest(request.requesterId)}>
-              <Text>Decline</Text>
+              <Text style={styles.acDcTxt}>Decline</Text>
             </TouchableHighlight>
           </View>
         ))}
@@ -166,8 +166,72 @@ const styles = StyleSheet.create({
 
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0b2f42',
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
+  mainText: {
+    alignSelf: 'center',
+    padding: 8,
+    borderRadius: 8,
+    fontWeight: '500',
+    fontSize: Platform.OS === 'web'? 18 : 9,
+    backgroundColor: '#0f4e70',
+    color: 'white',
+    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    alignContent: 'center',
+    margin: 10
+  },
+  head: {
+    fontSize: Platform.OS === 'web'? 30 : 15,
+    padding: 2,
+    color: '#D2B80F',
+    borderWidth: 4,
+    borderColor: '#134e6d',
+    borderRadius: 4,
+    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+    backgroundColor: '#0d384f',
+    textAlign: 'center',
+    marginBottom: 18,
+    marginTop: 8,
+    marginHorizontal: 41
+  },
+  inputText: {
+    alignSelf: 'center',
+    padding: 8,
+    borderRadius: 8,
+    fontWeight: '500',
+    fontSize: Platform.OS === 'web'? 18 : 9,
+    backgroundColor: '#0f4e70',
+    color: '#D2B80F',
+    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    margin: 10
+  },
+  reqText: {
+    alignSelf: 'center',
+    padding: 8,
+    borderRadius: 8,
+    fontWeight: '200',
+    fontSize: Platform.OS === 'web'? 14 : 7,
+    backgroundColor: '#0f4e70',
+    color: '#D2B80F',
+    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    margin: 10
+  },
+  acDcTxt: {
+    color: '#D2B80F',
+    marginVertical: 2,
+    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+    fontWeight: '400',
+    fontSize: Platform.OS === 'web'? 18 : 9,
+    textAlign: 'center'
+  }
 
 });
